548adba1e70899e87fc35f715b4ce09805fce36c
sulemanmalgave <sulemanmalgave1@gmail.com>
2026-06-14 15:53:40 +0530

    refactor: simplify asset paths for icons
    Move icon assets to the root directory and update references in HTML, manifest, and service worker to improve path resolution.

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,9 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/maskable-icon-512.png',
   '/screenshots/desktop.png',
   '/screenshots/mobile.png'
 ];
